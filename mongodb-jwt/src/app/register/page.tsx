9c094fe1c6bd8188362d34cc382545ae22e31cee
imran752310 <muhammadimran752310@gmail.com>
2025-07-13 23:02:20 +0500

    login for and getdata pass from APi

---
+++ mongodb-jwt/src/app/register/page.tsx
@@ -0,0 +1,12 @@
+import UserForm from '@/components/UserForm'
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div>
+        <UserForm />
+    </div>
+  )
+}
+
+export default Register
---
+++ mongodb-jwt/src/components/UserForm.tsx
@@ -0,0 +1,75 @@
+"use client"
+import React, { useState } from 'react'
+
+export default function UserForm() {
+
+ const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setMessage('');
+  console.log('Sending data:', { name, email, password });
+
+  try {
+    const res = await fetch('/api/register',{ method : " POST" , headers : {'Content-Type': 'application/json'}, body: JSON.stringify({name,email,password})})
+
+    const data = await res.json();
+      if (!res.ok) throw new Error(data.message || 'Something went wrong');
+
+      setMessage('User created successfully!');
+      setName('');
+      setEmail('');
+      setPassword('');
+
+  } catch (err: any) {
+      setMessage(err.message || 'Failed to create user');
+    }
+}
+
+  return (
+    <div>
+        <div className="max-w-md mx-auto mt-10 p-6 border rounded-xl shadow-md">
+      <h2 className="text-2xl font-bold mb-4">Register New User</h2>
+      <form onSubmit={handleSubmit} className="space-y-4">
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={e => setName(e.target.value)}
+          className="w-full p-2 border rounded"
+          required
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          className="w-full p-2 border rounded"
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          className="w-full p-2 border rounded"
+          required
+        />
+        <button
+          type="submit"
+          className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700"
+        >
+          Create Account
+        </button>
+      </form>
+      {message && (
+        <p className="mt-4 text-center text-sm text-red-600">{message}</p>
+      )}
+    </div>
+    </div>
+  )
+}
+
