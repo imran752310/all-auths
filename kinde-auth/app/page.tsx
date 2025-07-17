e21e6663cb8c05837933bf0b3eca27f8fdfa126c
imran752310 <muhammadimran752310@gmail.com>
2025-07-17 22:43:06 +0500

    complete kinde auth

--- kinde-auth/app/page.tsx
+++ kinde-auth/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
 
 export default function Home() {
   return (
    <div>
-    kinde auth
+ <LoginLink>Sign in</LoginLink>
+
+<RegisterLink>Sign up</RegisterLink>
    </div>
   );
 }
