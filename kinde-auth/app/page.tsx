f5f80cfabbfb352d7a0c1ced385b1fa33c681b38
imran752310 <muhammadimran752310@gmail.com>
2025-07-18 23:48:46 +0500

    ok

--- kinde-auth/app/page.tsx
+++ kinde-auth/app/page.tsx
@@ -2,7 +2,7 @@ import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
 
 export default function Home() {
   return (
-   <div>
+   <div className="w-[90%] mx-auto">
  <LoginLink>Sign in</LoginLink>
 
 <RegisterLink>Sign up</RegisterLink>
